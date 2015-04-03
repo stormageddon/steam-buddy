a1fa062d19fee43adea2fd4571a4e104a3a476cc
Michael Caputo <therubikscubekid@gmail.com>
2015-04-02 20:42:08 -0400

    Got message sending working and added multiple users. Ugly version 1.

--- steambuddy.js
+++ steambuddy.js
@@ -2,20 +2,27 @@ var request = require('request');
 
 var hostUrl = 'http://powerful-cliffs-9562.herokuapp.com';
 var hostPort = '80';
-//var hostPath = hostUrl + ':' + hostPort;
-var hostPath = '127.0.0.1:3000';
-//var sessionToken = 'ba716392dca54875977ef32f11a41165174f0dd61c13a79a2ec7bf59dd44509387b2dc8c1ab127ca79e250153f08cb52';
-var sessionToken = '4a758c0306e81c61f04f8cb367f602ef42bdad20860727a20fe6d417a858a45e51e53ff087b9e782e1791b60727c74e2'
+var hostPath = hostUrl + ':' + hostPort;
+var usersToCheck = ['76561198024956371'/*ethan*/, '76561198081408345'/*me*/, '76561198013610944' /*jared*/, '76561198009215427' /*dave*/];
+var currOnline = [];
+
+//var hostPath = '127.0.0.1:3000';
+var sessionToken = 'ba716392dca54875977ef32f11a41165174f0dd61c13a79a2ec7bf59dd44509387b2dc8c1ab127ca79e250153f08cb52';
+//var sessionToken = '4a758c0306e81c61f04f8cb367f602ef42bdad20860727a20fe6d417a858a45e51e53ff087b9e782e1791b60727c74e2'
 var io = require('socket.io-client');
-this.socket = null;
+this.socket = io.connect(hostPath, { query: "token="+sessionToken});
+
+var minutes = 30, the_interval = minutes * 60 * 1000; //30 seconds
+setInterval(function() {
+  for( var i = 0; i < usersToCheck.length; i++ ) {
+    var steamId = usersToCheck[i];
+    sendMessage(this.socket, steamId);
+  }
+}.bind(this), the_interval);
 
 this.connect = function(token) {
-  console.log('Connecting to Socket.io!');
-  this.socket = io.connect(hostPath, { query: "token="+token });
-  console.log('set socket',this.socket);
   this.socket.on('connect', function() {
     console.log('Connected!');
-    sendMessage(this.socket);
   });
 
   this.socket.on('error', function(err) {
@@ -37,28 +44,55 @@ this.connect(sessionToken);
 
 var API_KEY = '9186ADF14E6553A2257FAC4856F822EA';
 var TEST_ID = '76561198081408345'//ethan: '76561198024956371'; // me: '76561198081408345';//'76561198064286306';
-var TEST_URL = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=9186ADF14E6553A2257FAC4856F822EA&steamids='+TEST_ID;
-var sendMessage = function(socket) {
-  request(TEST_URL, function (error, response, body) {
+var TEST_URL = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=9186ADF14E6553A2257FAC4856F822EA&steamids=';
+
+
+var sendMessage = function(socket, steamIdToCheck) {
+  var url = TEST_URL + steamIdToCheck;
+  console.log('id to check:', steamIdToCheck);
+  request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var parsedResult = JSON.parse(body);
       var player = parsedResult.response.players[0];
+      var playerId = player.steamid;
       var playerName = player.personaname;
       var game = player.gameextrainfo;
-      console.log('%s is playing %s',playerName,game);
-      console.log('socket:',this.socket);
+
       if( game ) {
-	if( socket != null ) {
+	console.log('%s is playing %s',playerName,game);
+	if (socket != null && !userIsInGame(playerId)) {
+	  var groupId = '5341d9c9118f86020000000a';// test group: "5446efc342718e0200b33be1"; //Besties: 5341d9c9118f86020000000a
           var messageText = playerName + ' is playing ' + game + '! Go join him!';
-          var message = {from: '5442e50b36f60d020079e358', group: '5341d9c9118f86020000000a', hasMedia: false, sent: Date.now(), 'text': messageText};
+          var message = {group: groupId, hasMedia: false, 'text': messageText};
 	  notify(message, socket);
+	  currOnline.push(playerId);
 	}
 	else {
 	  console.log('not connected');
 	}
       }
+      else {
+	if (userIsInGame(playerId)) {
+	  console.log('remove from game');
+	  currOnline.splice(currOnline.indexOf(playerId), 1);
+	}
+	  
+      }
     }
   });
+}
+
+var userIsInGame = function(playerId) {
+  return currOnline.indexOf(playerId) >= 0;
+  /*var check = currOnline.filter(function( obj ) {
+    return obj._id === playerId;
+  });
+
+if( check.length ) {
+    console.log('yep');
+} else {
+    console.log('nope');
+}*/
 }
 
 var notify = function(message, socket) {
