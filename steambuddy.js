ba483a558913f8803d8b365f5333e2a597d8fa12
Michael Caputo <therubikscubekid@gmail.com>
2015-04-06 17:21:44 -0400

    keep alive

--- steambuddy.js
+++ steambuddy.js
@@ -29,7 +29,9 @@ setInterval(function() {
   }
 
   // keep alive
-  request('https://morning-shore-1795.herokuapp.com/' + (process.env.PORT || '3000'), function(error, response, body) {
+  var keepAlivePort = process.env.PORT || '3000';
+  var keepAlivePath = 'localhost' + keepAlivePort;
+  request(keepAlivePath, function(error, response, body) {
     if (error) {
       console.log('error:', error);
     }
