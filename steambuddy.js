2b9323c18e386c5b113f954226cd71233f53ade0
Michael Caputo <therubikscubekid@gmail.com>
2015-04-06 17:24:32 -0400

    fixed url

--- steambuddy.js
+++ steambuddy.js
@@ -30,7 +30,7 @@ setInterval(function() {
 
   // keep alive
   var keepAlivePort = process.env.PORT || '3000';
-  var keepAlivePath = 'localhost' + keepAlivePort;
+  var keepAlivePath = 'localhost:' + keepAlivePort;
   request(keepAlivePath, function(error, response, body) {
     if (error) {
       console.log('error:', error);
